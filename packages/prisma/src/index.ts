22c01636e1f69668649afaeade84e9d9e71f6248
deytapas <deytapas374@gmail.com>
2026-05-02 20:03:45 +0530

    try to fix

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
-console.log(process.env.DATABASE_URL);
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-export const prisma = new PrismaClient({ adapter });
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+// export const prisma = new PrismaClient({ adapter });
+
+import { PrismaClient } from "@prisma/client";
+// console.log(process.env.DATABASE_URL);
+export const prisma = new PrismaClient();
